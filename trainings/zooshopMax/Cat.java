ba00ae02b1510122802ee8596a826242f91cac61
MaximBelyj <belyjmaximteam3@gmail.com>
2016-12-08 00:11:55 +0300

    Наследовал класс Cat от Animal, с переопределением двух методов.

--- trainings/zooshopMax/Cat.java
+++ trainings/zooshopMax/Cat.java
@@ -1,41 +1,14 @@
 package zooshop;
 
-public class Cat {
+public class Cat extends Animal {
     
-   private double lengthCat; //длина котов/кошек  
-   private int ageCat;//возраст котов/кошек
-   private String nameCat;//Клички животных
-   
-   public void setLengthCat(double l){ //задаем размер
-       this.lengthCat = l;
-   }
-   
-   public void setAgeCat(int a){//задаём возраст
-       this.ageCat = a;
-   }
-   
-   public void setNameCat(String n){//задаём имя
-       this.nameCat = n;
-   }
-   
-   public double getLengthCat(){
-       return lengthCat;
-   }
-   
-   public int getAgeCat(){
-       return ageCat;
-   }
-   
-   public String getNameCat(){
-       return nameCat;
-   }
-   
-   public void feedCat(){
-       System.out.println("You feed cat!");
-   }
-   
-   public void playWithCat(){
-       System.out.println("You play with cat!");
-   }
-   
+    @Override
+    public void feed(){ //покормить животное
+        System.out.println("You feed cat!");
+    }
+    
+    @Override
+    public void playWithAnimal(){ // поиграть с животным
+        System.out.println("You play with cat!");
+    }
 }
