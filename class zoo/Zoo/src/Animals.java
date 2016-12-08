4447e258cb5e587ded8561d3d4caf3970ff62c55
nikorekhov <orekhovnikolay@mail.ru>
2016-12-08 11:00:46 +0300

    Класс животных

---
+++ class zoo/Zoo/src/Animals.java
@@ -0,0 +1,27 @@
+/**
+ * Created by user on 03.12.2016.
+ */
+public class Animals {
+
+    private String name;
+
+    public void evolute()
+    {
+        System.out.println("This animal was upgrated!");
+    }
+
+    public void getName()
+    {
+        System.out.print(this.name);
+    }
+
+    public void sleep()
+    {
+        System.out.println("Zzzzzz...");
+    }
+
+    public void voice()
+    {
+        System.out.println(" ... ");
+    }
+}
