3decb1530d7c584f91b4188d217f6f2d314a1346
MaximBelyj <belyjmaximteam3@gmail.com>
2016-12-09 16:34:20 +0300

    Тоже что и для котов

--- trainings/zooshopMax/Dog.java
+++ trainings/zooshopMax/Dog.java
@@ -2,6 +2,12 @@ package zooshop;
 
 public class Dog extends Animal{
     
+    public Dog (String name, String colour, int age, double length, double weight){
+        super(name,colour,age,length,weight);
+    }
+    
+    public Dog(){}
+    
     @Override
     public void feed(){ //покормить животное
         System.out.println("You feed dog!");
