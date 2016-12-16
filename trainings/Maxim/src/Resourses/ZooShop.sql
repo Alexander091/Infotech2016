4ac1ebfcbe9cb480d45fd1a0212fbec747650659
MaximBelyj <belyjmaximteam3@gmail.com>
2016-12-16 12:29:38 +0300

    Изменил название

---
+++ trainings/Maxim/src/Resourses/ZooShop.sql
@@ -0,0 +1,14 @@
+INSERT INTO params (attr_id, object_id, value)
+            VALUES (1, 2, 8);
+
+INSERT INTO params (attr_id, object_id, value)
+VALUES (2, 2, 2);
+
+INSERT INTO params (attr_id, object_id, value)
+VALUES (3, 2, 5);
+
+INSERT INTO params (attr_id, object_id, value)
+VALUES (4, 2, 'Sharic');
+
+INSERT INTO params (attr_id, object_id, value)
+VALUES (5, 2, 'Black');
