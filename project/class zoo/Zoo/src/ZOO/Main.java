5758d062e44eca8aaec4782ac48eae85922a8bc3
nikorekhov <orekhovnikolay@mail.ru>
2016-12-20 00:39:05 +0300

    Точка входа.

--- project/class zoo/Zoo/src/ZOO/Main.java
+++ project/class zoo/Zoo/src/ZOO/Main.java
@@ -4,63 +4,64 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Created by user on 08.12.2016.
- */
 public class Main {
 
-
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException{
         Zoo zooshop = new Zoo();
         Cat cat = new Cat(1,false, "Siamese", "Murka");
         Dog dog = new Dog(2,true,"Mops", "Sharik");
-
-        zooshop.buyCat(cat);
-        zooshop.buyDog(dog);
-
+        zooshop.buyCat(4,"Murka");
+        zooshop.buyDog(5,"Sharik");
         Menu(cat,dog,zooshop);
-
     }
 
     protected static void Menu(Cat cat, Dog dog, Zoo zooshop) throws IOException{
 
         boolean flag = true;
-        int numSellList = 0;
+        String name;
         System.out.println("Вас приветствует Зоомагазин.");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (flag) {
-            System.out.println("\n0 - добавить кошку в магазин.\n" + "1 - добавить собаку в магазин\n" + "2 - удалить животное из магазинв\n" + "3 - посмотреть список животных магазина\n" + "10 - выйти из магазина");
+            System.out.println("\n0 - добавить кошку в магазин.\n" + "1 - добавить собаку в магазин\n" + "2 - удалить кошку из магазина\n" + "3 удалить собаку из магазина\n" + "4 - посмотреть список животных магазина\n" + "10 - выйти из магазина");
             int key = Integer.parseInt(reader.readLine());
             switch (key){
                 case 0: {
-                    Cat.addnewAnimal(cat);
-                    zooshop.buyCat(cat);
+                    name = Cat.addnewAnimal(cat);
+                    zooshop.buyCat(4,name);
                     break;
                 }
                 case 1: {
-                    Dog.addnewAnimal(dog);
-                    zooshop.buyDog(dog);
+                    name = Dog.addnewAnimal(dog);
+                    zooshop.buyDog(5,name);
                     break;
                 }
                 case 2: {
                     zooshop.printInfo();
-                    numSellList = Integer.parseInt(reader.readLine());
-                    zooshop.sellCat(numSellList);
+                    System.out.println("Введите имя животного: ");
+                    name = reader.readLine();
+                    zooshop.sellCat(name);
                     break;
                 }
                 case 3: {
-                    zooshop.printIformation();
                     zooshop.printInfo();
+                    System.out.println("Введите имя животного: ");
+                    name = reader.readLine();
+                    zooshop.sellDog(name);
+                    break;
+                }
+                case 4: {
+                    zooshop.printInfo();
+                    zooshop.printAnimal();
                     break;
                 }
                 case 10: {
-                    zooshop.printIformation();
+                    zooshop.printInfo();
                     flag = false;
                     break;
                 }
                 default: {
                     System.out.println("Что-то пошло не так...\nКонец программы...");
+                    zooshop.closeShop();
                     flag = false;
                     break;
                 }
